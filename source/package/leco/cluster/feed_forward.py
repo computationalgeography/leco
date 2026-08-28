b52ca228dcf28529ea3453a036ffff2e155d169e
AnkePosma <anke.posma@gmail.com>
2026-08-28 10:00:57 +0200

    Fixes gpkg extension #35

--- source/package/leco/cluster/feed_forward.py
+++ source/package/leco/cluster/feed_forward.py
@@ -723,8 +723,14 @@ def diversify(
         ]
 
     # Save output to a single gpkg file
+    output_path = directory / gpkg_file_name
+    if output_path.suffix == "":
+        # Add .gpkg suffix to output path if not present
+        output_path = output_path.with_suffix(".gpkg")
+        logger.warning("Warning: adding .gpkg suffix to output path: %s", output_path)
+
     pd.concat(population_total, ignore_index=True).to_file(
-        directory / gpkg_file_name,
+        output_path,
         driver="GPKG",
     )
 
